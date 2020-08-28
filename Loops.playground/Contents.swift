153bac86da02d7b960f9ad39b9670e13ab55f512
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-29 00:32:16 +0530

    For Loop in Swift

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -2,4 +2,27 @@ import UIKit
 
 // Loops in Swift
 
+// **************** For Loop *****************
+let count = 1...10
+for number in count {
+    print("Number is \(number)")
+}
+
+// For Loop with Array
+let hackerNames = ["MRX", "Clay","Benjamin", "Elliot Anderson", "willskhalifa"]
+for hacker in hackerNames {
+    print("hacker \(hacker)")
+}
+
+// without constant we should use _ so that swift doesn't create needless value
+print("Dancer gonna dance")
+for _ in 1...5{
+    print("Dance")
+}
+
+// for a particular range and increment
+for i in stride(from: 0, to: 10, by: 1){
+    print(i)
+}
+
 
